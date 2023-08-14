6f024a945c293fc463445d886f620781b10c25e7
iti-0204 <yasuken0327@icloud.com>
2023-08-14 15:37:21 +0900

    types追加、useAuth適用前

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,32 @@
 import axios from "axios";
 import React, { useCallback } from "react";
 
+import { User } from "../types/api/user";
+import { useHistory } from "react-router-dom";
+
 export const useAuth = () => {
-  const login = useCallback((email:string) => {
-    axios
-  }, []);
-  return login;
+  let match = false;
+  const history = useHistory();
+  const login = useCallback(
+    (email: string) => {
+      axios
+        .get("/apiapp/profile/")
+        .then((res) => {
+          res.data.forEach(function (value: any) {
+            console.log(email);
+            if (value.id == email) {
+              match = true;
+              history.push("/home");
+            }
+          });
+          // if (match) {
+          //   history.push("/home");
+          // }
+        })
+        .catch((err: any) => console.log(err))
+        .finally(() => console.log("finally"));
+    },
+    [history]
+  );
+  return { login };
 };
