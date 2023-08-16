60b6dfec8b93813228a954ccde9f0ce846ba5ddd
iti-0204 <yasuken0327@icloud.com>
2023-08-16 18:14:18 +0900

    タッチしたeventモーダル表示

--- src/components/organisms/layout/Calendar.tsx
+++ src/components/organisms/layout/Calendar.tsx
@@ -164,9 +164,56 @@ const InputForm = (props: any) => {
   );
 };
 
+const InputForm2 = (props: any) => {
+  return (
+    <>
+      <ModalStyle>
+        <FormLabel fontSize="16px" flexBasis="25%" margin="0px">
+          {props.title}
+        </FormLabel>
+        <Input
+          placeholder={props.placeholder}
+          variant="unstyled"
+          flexBasis="75%"
+          textAlign="right"
+          disabled
+          _placeholder={{ opacity: 1, color: "black" }}
+          _disabled={{ opacity: 1 }}
+        ></Input>
+      </ModalStyle>
+    </>
+  );
+};
+
+const InputInUnder = (props: any) => {
+  return (
+    <>
+      <FormLabel fontSize="16px" flexBasis="25%" margin="0px">
+        {props.title}
+      </FormLabel>
+      <Input
+        placeholder={props.placeholder}
+        variant="unstyled"
+        flexBasis="75%"
+        textAlign="right"
+        // value={props.value}
+        // onChange={props.onChange}
+        disabled
+        _placeholder={{ opacity: 1, color: "black" }}
+        _disabled={{ opacity: 1 }}
+      ></Input>
+    </>
+  );
+};
+
 export const Calendar = (): JSX.Element => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const onClickSelect = () => onOpen();
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const modal1 = useDisclosure();
+  const modal2 = useDisclosure();
+  const onClickSelect = modal1.onOpen;
+
+  const onClickEvent = modal2.onOpen;
 
   const { PostSchedule } = useSchedule();
 
@@ -174,14 +221,20 @@ export const Calendar = (): JSX.Element => {
   // 開始時間と終了時間
   const [startdate, setStartDate] = useState(Today);
   const [stringstartdate, setStringStartDate] = useState("");
+  const [event_start, setEvent_start] = useState(Today);
+
   const [enddate, setEndDate] = useState(Today);
   const [stringenddate, setStringEndDate] = useState("");
+  const [event_end, setEvent_end] = useState(Today);
   //タイトル
   const [title, setTitle] = useState("");
+  const [event_title, setEvent_title] = useState("");
   //予算
   const [budget, setBudget] = useState("");
+  const [event_budget, setEvent_budget] = useState("");
   //部屋番号
   const [password, setPassword] = useState("");
+  const [event_password, setEvent_password] = useState("");
 
   const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -228,6 +281,23 @@ export const Calendar = (): JSX.Element => {
     border-bottom: 1px solid #d9d9d9;
   `;
 
+  const events = [
+    {
+      id: "a", // ユニークID
+      start: "2023-08-02", // イベント開始日
+      end: "2023-08-02", // イベント終了日
+      starttime: "2023-08-02", // イベント開始日
+      endtime: "2023-08-02", // イベント終了日
+      title: "節分", // イベントのタイトル
+      password: "4040",
+      budget: "10000",
+      backgroundColor: "red", // 背景色
+      borderColor: "red", // 枠線色
+      editable: true, // イベント操作の可否
+    },
+    // 省略
+  ];
+
   return (
     <>
       <StyleWrapper>
@@ -239,7 +309,7 @@ export const Calendar = (): JSX.Element => {
           initialView="dayGridMonth"
           locales={[jaLocale]}
           locale="ja"
-          // initialEvents={events}
+          initialEvents={events}
           selectable={true}
           headerToolbar={{
             start: "prev",
@@ -247,13 +317,21 @@ export const Calendar = (): JSX.Element => {
             end: "next",
           }}
           select={onClickSelect}
+          // eventClick={onClickEvent}
+          eventClick={(e) => {
+            // event_title = e.event._def.title;
+            setEvent_title(e.event._def.title);
+            setEvent_start(e.event._def.extendedProps.starttime);
+            setEvent_end(e.event._def.extendedProps.endtime);
+            setEvent_budget(e.event._def.extendedProps.budget);
+            setEvent_password(e.event._def.extendedProps.password);
+            console.log(typeof event_title);
+            onClickEvent();
+          }}
           height="600px"
-
-          // businessHours={{ daysOfWeek: [1, 2, 3, 4, 5] }}
-          // weekends={true}
         />
       </StyleWrapper>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modal1.isOpen} onClose={modal1.onClose}>
         <ModalOverlay />
         <ModalContent
           w="350px"
@@ -297,12 +375,6 @@ export const Calendar = (): JSX.Element => {
                       minDate={Today}
                       onChange={onChangeStringStartDate}
                     />
-                    {/* <Input
-                      placeholder="開始時間"
-                      variant="unstyled"
-                      flexBasis="75%"
-                      textAlign="right"
-                    ></Input> */}
                   </ModalStyleUnderbar>
                 </FormControl>
                 {/* 終了時間 */}
@@ -318,12 +390,6 @@ export const Calendar = (): JSX.Element => {
                       minDate={Today}
                       onChange={onChangeStringEndDate}
                     />
-                    {/* <Input
-                      placeholder="終了時間"
-                      variant="unstyled"
-                      flexBasis="75%"
-                      textAlign="right"
-                    ></Input> */}
                   </ModalStyle>
                 </FormControl>
               </Box>
@@ -343,48 +409,6 @@ export const Calendar = (): JSX.Element => {
                   onChange={onChangePassword}
                 />
               </FormControl>
-              {/* 貢献度
-              <FormControl>
-                <ModalStyle>
-                <FormLabel fontSize="16px" flexBasis="25%" margin="0px">
-                  貢献度
-                </FormLabel>
-                <Input
-                  placeholder="貢献度"
-                  variant="unstyled"
-                  flexBasis="75%"
-                  textAlign="right"
-                ></Input>
-                </ModalStyle>
-              </FormControl> */}
-              {/* 場所
-              <FormControl>
-                <ModalStyle>
-                <FormLabel fontSize="16px" flexBasis="25%" margin="0px">
-                  場所
-                </FormLabel>
-                <Input
-                  placeholder="場所"
-                  variant="unstyled"
-                  flexBasis="75%"
-                  textAlign="right"
-                ></Input>
-                </ModalStyle>
-              </FormControl>
-              集合
-              <FormControl>
-                <ModalStyle>
-                <FormLabel fontSize="16px" flexBasis="25%" margin="0px">
-                  集合
-                </FormLabel>
-                <Input
-                  placeholder="集合"
-                  variant="unstyled"
-                  flexBasis="75%"
-                  textAlign="right"
-                ></Input>
-                </ModalStyle>
-              </FormControl> */}
             </VStack>
           </ModalBody>
           <ModalFooter justifyContent="center">
@@ -408,6 +432,78 @@ export const Calendar = (): JSX.Element => {
                 onClick={onClickPostSchedule}
               >
                 決定
+              </Button>
+            </VStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {/* 登録した予定のモーダル --------------------------------------------------------------*/}
+
+      <Modal isOpen={modal2.isOpen} onClose={modal2.onClose}>
+        <ModalOverlay />
+        <ModalContent
+          w="350px"
+          backgroundColor="baseColors.beige"
+          borderRadius="15px"
+        >
+          <ModalHeader
+            textAlign="center"
+            backgroundColor="baseColors.blue"
+            color="white"
+            height="48px"
+            borderTopLeftRadius="15px"
+            borderTopRightRadius="15px"
+            fontSize="18px"
+            alignItems="center"
+            py="10px"
+          >
+            {event_title}
+          </ModalHeader>
+          <ModalCloseButton color="white" />
+          <ModalBody padding="0px" mt="45px">
+            <VStack spacing="15px">
+              {/* 開始時間 */}
+              <Box w="100%">
+                <FormControl>
+                  <ModalStyleUnderbar>
+                    <InputInUnder title="開始" placeholder={event_start} />
+                  </ModalStyleUnderbar>
+                </FormControl>
+                {/* 終了時間 */}
+                <FormControl>
+                  <InputForm2 title="終了" placeholder={event_end} />
+                </FormControl>
+              </Box>
+              {/* 予算 */}
+              <FormControl>
+                <InputForm2 title="予算" placeholder={event_budget} />
+              </FormControl>
+              {/* 部屋番号 */}
+              <FormControl>
+                <InputForm2 title="部屋番号" placeholder={event_password} />
+              </FormControl>
+            </VStack>
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            <VStack spacing="34px" align="center">
+              <Button
+                bgColor="baseColors.gray.100"
+                w="106px"
+                h="34px"
+                fontWeight="medium"
+                fontSize="14px"
+              >
+                編集
+              </Button>
+              <Button
+                bgColor="baseColors.yellow"
+                w="130px"
+                h="36px"
+                fontWeight="medium"
+                fontSize="14px"
+                color="white"
+              >
+                支払い入力画面へ
               </Button>
             </VStack>
           </ModalFooter>
